d720eab1c3a2ccd68b3a8585a41f9c22351421c2
Stephan Kulla <git.mail@kulla.me>
2017-06-30 17:59:59 +0200

    Move license into the header of each source file.

--- Makefile
+++ Makefile
@@ -1,3 +1,15 @@
+# Makefile for creating the presentation.
+#
+# Written in 2017 by Stephan Kulla
+#
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain
+# worldwide. This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along
+# with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
+
 .PHONY: all
 all: slides.pdf
 
--- continuous_slide_building.sh
+++ continuous_slide_building.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 #
 # Continuously builds the slides when the source files are changed.
+#
+# Written in 2017 by Stephan Kulla
+#
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain
+# worldwide. This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along
+# with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 # Build the PDF when it does not exist
 make
